01b607cd648956653d4616d34d31804ad1dea994
AinRuthPai <tanbing@naver.com>
2022-02-04 23:32:48 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import Page from "./components/Page";
+import { ThemeContext } from "./context/ThemeContext";
+import { UserContext } from "./context/UserContext";
 
 function App() {
-  return;
+  const [isDark, setIsDark] = useState(false);
+  return (
+    <UserContext.Provider value={"사용자"}>
+      <ThemeContext.Provider value={{ isDark, setIsDark }}>
+        <Page />
+      </ThemeContext.Provider>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
